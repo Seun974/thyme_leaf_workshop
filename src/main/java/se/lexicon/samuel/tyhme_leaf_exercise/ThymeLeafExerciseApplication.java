864a4c7e0350c4aebbad3de48ba93d37f4fea48c
Seun Bond <sadetoye@gmail.com>
2021-03-23 11:18:00 +0100

    styles sheet added

--- src/main/java/se/lexicon/samuel/tyhme_leaf_exercise/ThymeLeafExerciseApplication.java
+++ src/main/java/se/lexicon/samuel/tyhme_leaf_exercise/ThymeLeafExerciseApplication.java
@@ -2,9 +2,23 @@ package se.lexicon.samuel.tyhme_leaf_exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class ThymeLeafExerciseApplication {
+public class ThymeLeafExerciseApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // Register resource handler for images
+        registry.addResourceHandler("/img/**").addResourceLocations("/resources/static/img/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ThymeLeafExerciseApplication.class, args);
